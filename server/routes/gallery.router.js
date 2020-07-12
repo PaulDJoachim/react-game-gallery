5163e03cf07486643a0b13f9c3c20a66d035b420
Paul Joachim <PaulDJoachim@gmail.com>
2020-07-12 16:42:44 -0500

    database PUT working, database function ready to merge

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,23 +5,22 @@ const pool = require('../modules/pool')
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
+
 // PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+router.put( '/like/:id', (req, res)=>{
+  console.log( '/like PUT:', req.params.id);
+  const queryString = `UPDATE gallery SET likes = (likes + 1) WHERE id=${ req.params.id };`;
+  pool.query( queryString ).then( ( results )=>{
+      res.sendStatus( 200 );
+  }).catch( ( err )=>{
+      console.log( err );
+      res.sendStatus( 500 );
+  })
+}) 
 
-// // GET Route
-// router.get('/', (req, res) => {
-//     res.send(galleryItems);
-// }); // END GET Route
 
+
+// GET Route
 router.get('/',(req, res)=>{
   console.log('GETing list from database')
   let queryString = `SELECT * FROM gallery ORDER BY id ASC`;
